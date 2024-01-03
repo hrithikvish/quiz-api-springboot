76b4370cfab869169c4092ab0a4bb9f8ab9de4b5
hrithikvish <hrithikvishwakarma05@gmail.com>
2024-01-03 22:39:18 +0530

    Initial Commit

---
+++ src/main/java/com/hrithikvish/springbootquizapp/controller/QuestionController.java
@@ -0,0 +1,44 @@
+package com.hrithikvish.springbootquizapp.controller;
+
+import com.hrithikvish.springbootquizapp.Question;
+import com.hrithikvish.springbootquizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions") //@RequestMapping(value="allQuestions", method=RequestMethod.GET)
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @GetMapping("{id}")
+    public Optional<Question> getQuestion(@PathVariable int id) {
+        return questionService.getQuestionById(id);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("del/{id}")
+    public String delQuestion(@PathVariable int id) {
+        return questionService.delQuestion(id);
+    }
+
+}
---
+++ src/main/java/com/hrithikvish/springbootquizapp/service/QuestionService.java
@@ -0,0 +1,58 @@
+package com.hrithikvish.springbootquizapp.service;
+
+import com.hrithikvish.springbootquizapp.Question;
+import com.hrithikvish.springbootquizapp.dao.QuestionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDAO questionDAO;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDAO.findAll(), HttpStatus.OK);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDAO.findByCategory(category), HttpStatus.OK);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public Optional<Question> getQuestionById(int id) {
+        return questionDAO.findById(id);
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionDAO.save(question);
+            return new ResponseEntity<>("Question Added Successfully!", HttpStatus.CREATED);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Question not added.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public String delQuestion(int id) {
+        questionDAO.deleteById(id);
+        return "Question Deleted Successfully!";
+    }
+}
